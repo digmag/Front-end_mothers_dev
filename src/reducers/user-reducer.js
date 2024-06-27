1a3070d81d26f0e31ce7ac9819bdf3bc0636f2bb
Kseniya Yakovleva <ksushaykovleva2004@gmail.com>
2024-06-27 09:59:58 +0700

    Workers

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -2,13 +2,22 @@ const LOGIN = "LOGIN";
 const IS_ADMIN = "IS_ADMIN";
 const STATUS_LIST = "STATUS_LIST";
 const ADD_STATUS = "ADD_STATUS";
+const WORKERS = "WORKERS"
 
 const initialUserState = {
     isAuth: 0,
     isAdmin: 0,
     statusList: [
 
-    ]
+    ],
+    workers: [{
+        id: "",
+        fullName: "",
+        email: "",
+        status: "",
+        admin: false,
+        verification: false
+    }]
 }
 
 const userReducer = (state = initialUserState, action) => {
@@ -43,6 +52,14 @@ const userReducer = (state = initialUserState, action) => {
             newState.statusList = [...newState.statusList, { ...action.data }];
             return newState;
 
+        case WORKERS:
+            const arrayy = []
+            action.workers.forEach(el => {
+                arrayy.push({ value: el.id, label: el.fullName })
+            })
+            newState.workers = arrayy;
+            return newState;
+
 
         default:
             return newState;
@@ -63,6 +80,10 @@ export function statusListActionCreator(data) {
 
 export function addStatusActionCreator(data) {
     return { type: ADD_STATUS, data: data }
+}
+
+export function workersActionCreator(workers) {
+    return { type: WORKERS, workers: workers }
 }
 
 
